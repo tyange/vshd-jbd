1dcc6cf020de268bedbd6828e17555f1da69b49f
tyange <usun16@gmail.com>
2025-02-27 08:00:25 +0900

    testing connect supabase storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
-  return (
-    <div>
-      <p className="text-red-500">hi</p>
-    </div>
-  );
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.storage.from("photos").list("");
+  console.log(data);
+
+  return <div>{data && data.map((d) => <div key={d.id}>{d.name}</div>)}</div>;
 }
